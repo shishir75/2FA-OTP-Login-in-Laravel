e6ccfff3cf31bd3db0d9c1ef11bd33e7dd94ad9e
shishir75 <shishir.srdr16@gmail.com>
2020-08-16 01:37:05 +0600

    Handle Invalid OTP

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,4 +61,30 @@ class LoginController extends Controller
 
         return $result;
     }
+
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        auth()->user()->update(['isVerified' => 0]);
+
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new Response('', 204)
+            : redirect('/');
+    }
 }
--- resources/views/OTP/verify.blade.php
+++ resources/views/OTP/verify.blade.php
@@ -7,12 +7,20 @@
             <div class="card">
                 <div class="card-header">Enter OTP</div>
 
+                @if ($errors->count() > 0)
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            {{ $error }}
+                        @endforeach
+                    </div>
+                @endif
+
                 <div class="card-body">
                     <form action="/verifyOTP" method="POST">
                         @csrf
                         <div class="form-group">
                             <label for="otp">Your OTP</label>
-                            <input type="text" class="form-control" name="otp" id="otp" required>
+                            <input type="text" class="form-control" name="OTP" id="otp" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
